3bc5dc38dfb7d12ecc6a8442b969a74c2c9f4905
s1101137209 <zxc778899asd@gmail.com>
2015-05-01 17:41:20 +0800

    second

--- KuasCore/Interceptors/DebogLogInterceptor.cs
+++ KuasCore/Interceptors/DebogLogInterceptor.cs
@@ -10,12 +10,12 @@ namespace KuasCore.Interceptors
         public object Invoke(IMethodInvocation invocation)
         {
             //Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
-            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            //Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
 
             object result = invocation.Proceed();
 
-            Console.WriteLine("回傳的資料已取得 [{0}]", result);
-            Debug.Print("回傳的資料已取得 [{0}]", result);
+            //Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            //Debug.Print("回傳的資料已取得 [{0}]", result);
 
             return result;
         }
